32596125833e159b4a45b89b3791c51103c39d7f
cortega <cortega@acciontip.cl>
2023-01-16 20:13:54 -0600

    style

--- src/components/main/slider/index.jsx
+++ src/components/main/slider/index.jsx
@@ -6,18 +6,21 @@ import imgSlider02 from "../../../svg/img/slider02.jpg";
 import imgSlider03 from "../../../svg/img/slider03.jpg";
 import imgSlider04 from "../../../svg/img/slider04.jpg";
 import imgSlider05 from "../../../svg/img/slider05.jpg";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Slider = () => {
   const selector = useSelector((state) => state.scrollReducer);
 
-  const CardSlider = ({ url, name, img }) => {
+  const CardSlider = ({ url, name, img, info }) => {
 
     return (
       <div
         className="divContainerSlider02 flexColumn"
       >
         <h2>{name}</h2>
+        <div className="divContainerSlider03 flexColumn">
+{info}
+        </div>
         <button
           onClick={() => {
             window.open(url, "_blank");
@@ -25,6 +28,7 @@ const Slider = () => {
         >
           Visitar
         </button>
+  
         <img src={img}></img>
       </div>
     );
@@ -42,36 +46,42 @@ const Slider = () => {
           name={"Portafolio"}
           img={imgSlider00}
           url={"https://portafolio-frontend-drab.vercel.app/"}
+          info={"Html5, css3, React.js, typeScript, node.js"}
         />
         <CardSlider
           numCard={1}
           name={"template"}
           img={imgSlider01}
           url={"https://test-seven-psi-61.vercel.app/"}
+          info={"Html5, css3, Next.js"}
         />
         <CardSlider
           numCard={2}
           name={"template"}
           img={imgSlider02}
           url={"https://test-propulso.vercel.app/#"}
+          info={"Html5, css3"}
         />
         <CardSlider
           numCard={3}
           name={"template"}
           img={imgSlider03}
           url={"https://prueba04.vercel.app/"}
+          info={"Html5, css3"}
         />
         <CardSlider
           numCard={4}
           name={"template"}
           img={imgSlider04}
           url={"https://datos-pais.vercel.app/"}
+          info={"Html5, css3, React.js"}
         />
         <CardSlider
           numCard={5}
           name={"template "}
           img={imgSlider05}
           url={"https://cugat-publicidad-kleg.vercel.app/"}
+          info={"Html5, css3, javascript"}
         />
       </div>
     </div>
