1c73253d3d64f30f0dc917b096fbb6f2ef9525e2
cortega <cortega@acciontip.cl>
2023-01-19 14:32:59 -0600

    style general

--- src/components/main/technologies/index.jsx
+++ src/components/main/technologies/index.jsx
@@ -1,11 +1,14 @@
 import "../../../styles/main/technologies/index.css";
 import "../../../styles/main/technologies/responsiveIndex.css";
-import imgHtml5_00 from "../../../svg/img/html.svg";
-import imgCss3_00 from "../../../svg/img/css.svg";
-import imgJavascript_00 from "../../../svg/img/js.svg";
-import imgReact_00 from "../../../svg/img/react.svg";
-import imgNode_00 from "../../../svg/img/node.svg";
+
+import imgCss3_00 from "../../../svg/img/css-3.svg";
+import imgJavascript_00 from "../../../svg/img/javascript-1.svg";
+import imgReact_00 from "../../../svg/img/react-2.svg";
+import imgNode_00 from "../../../svg/img/nodejs-icon.svg";
 import imgExpress_00 from "../../../svg/img/express.svg";
+
+import imgHtml5_00 from "../../../svg/img/html-1.svg";
+
 import svg from "../../../svg/index";
 import { useState } from "react";
 import { useSelector } from "react-redux";
@@ -73,7 +76,7 @@ const Technologies = () => {
                   <h4>{list.title}</h4>
                   <div className="divTechnologies04">
                     <img className="imgTechnologies00" src={list.img00}></img>
-                    <div>{list.img01}</div>
+                  {/*   <div>{list.img01}</div> */}
                   </div>
                 </div>
               );
