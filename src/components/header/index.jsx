dd380714b9f633c5f3a80bd2752854a087cdee92
cortega <cortega@acciontip.cl>
2023-02-07 03:43:22 -0300

    style svg

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,6 +4,7 @@ import svgIllustrations from "../../svg/index.jsx";
 import curriculum from "../../pdf/CVCRISTOBALORTEGA.pdf";
 
 import curriculumIco from "../../svg/img/briefcase_icon.svg"
+import pcIco from "../../svg/img/develop_development_web_icon.svg"
 import { useSelector } from "react-redux";
 
 const Header = () => {
@@ -15,11 +16,12 @@ const Header = () => {
     <header id="home" className="flexColumn">
       <div className="divContainerHeader00 flexRow">
         <div style={selector.homeMovi00} className="divContainerHeader01 flexColumn">
-          <h1>Developer</h1>
+          <h1><img style={{width:"4rem", height:"4rem"}} src={pcIco}></img> Developer</h1>
           <h1>Cristóbal Ortega</h1>
           <h1></h1>
           <a href={curriculum} download>
             <button >Descargar Curriculum</button>
+            <image src={curriculumIco}></image>
           </a>
         </div>
         <div className="divContainerHeader02">
